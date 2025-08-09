372d3dc6d719f2c7dd2426536148d6534a8c9210
lch24 <lch24tsinghua@qq.com>
2025-08-09 20:28:46 +0800

    Update run.sh

--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,11 @@
-# Add your commands here
+docker build -t hw_vol_image .
+
+if [ $# -lt 1 ]; then
+  echo "使用方法: $0 <源文件名.cpp> [程序参数...]"
+  exit 1
+fi
+
+SRC_FILE=$1
+shift
+
+docker run --rm -v "$(pwd)/../src:/app" hw_vol_image sh -c "g++ $SRC_FILE -o ${SRC_FILE%.cpp} && ./${SRC_FILE%.cpp} \"$@\""# Add your commands here
